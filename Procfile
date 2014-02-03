f7c28c1dc645147fcf638309b8a110da19d5addb
pradeep <pradeep@pradeeps-MacBook-Pro.local>
2014-02-03 14:20:50 +0530

    init

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node box_server.js
---
+++ box_server.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var url = require('url');
+var port = process.env.PORT || 3000;
+var logs=[];
+var server = http.createServer(function(req, res){
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+
+	if (req.url == "/log"){
+		logs.push(res)
+		console.log('added to the log array');
+	} else {
+		var query = url.parse(req.url).query
+		if (query != undefined){
+			console.log('got a query for %s', query);
+
+			for (var i=0;i<logs.length;i++) {
+				logs[i].write(query);
+				logs[i].write('\n');
+			}
+		}
+		res.end('ok');
+	}
+});
+server.listen(port);
+console.log('server running at %s', port);
